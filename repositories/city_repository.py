fbd6a99eadd1085802008fa33fd2e907e88bee4d
effrancina <f.v.menichetti@gmail.com>
2022-11-11 13:48:36 +0000

    added save to city repo

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -1,4 +1,11 @@
 from db.run_sql import run_sql
-# from models.city import City
-# from models.restaurant import Restaurant
 
+
+def save(city):
+    sql = """INSERT INTO cities (name, been_to)
+    VALUES (%s, %s) RETURNING *"""
+    values = [city.name, city.been_to]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    city.id = id
+    return results
